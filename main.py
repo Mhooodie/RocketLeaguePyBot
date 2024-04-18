29e784962cddca2460993f51e5a9bac713b400a1
Mhooodie <josephmcu2005@gmail.com>
2024-04-17 19:20:27 -0500

    Beats Rookie Bot

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-# This file is for strategy
 from util.objects import *
 from util.routines import *
 
 class Bot(GoslingAgent):
-    # This function runs every in-game tick (every time the game updates anything)
     def run(self):
+        d1 = abs(self.ball.location.y - self.foe_goal.location.y) # Distance Ball to Goal
+        d2 = abs(self.me.location.y - self.foe_goal.location.y) # Distance Me to Goal
+        is_infront_ball = d1 > d2
         if self.kickoff_flag:
             self.set_intent(kickoff())
             return
-        self.set_intent(atba())
+        if is_infront_ball:
+            self.set_intent(goto(self.friend_goal.location))
+            return
+        self.set_intent(short_shot(self.foe_goal.location))
